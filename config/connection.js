fae5ff32f80800e8cc3b8385aaafa8d582b3709b
Derek Stewart <derek.charles.stewart@gmail.com>
2019-10-27 10:47:55 -0400

    final

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
   host: "localhost",
-  port: 8080,
+  port: 3306,
   user: "root",
   password: "Yankees23",
   database: "burgers_db"
